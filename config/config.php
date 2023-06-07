49749232b2e5767127b1d2ecef507f6da0d2063f
GiangHT <121593208+p3rplexed@users.noreply.github.com>
2023-06-08 00:27:42 +0700

    config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 define("DB_HOST", 'localhost');
-define("DB_USER", 'root');
-define("DB_PASS", '');
+define("DB_USER", 'lab_root');
+define("DB_PASS", '123456');
 define("DB_NAME", 'DBS401');
 ?>
